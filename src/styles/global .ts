ec25a1415c7afd669c839fccd8fbab0fe6582f0a
Yan Cardoso <yancard7@gmail.com>
2022-03-11 15:05:56 -0300

    header component

---
+++ src/styles/global .ts
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --background: #F0F2F5;
+        --text: #969CB2;
+        --title: #363F5F;
+        --shape: #FFFFFF;
+        --red: #E52E4D;
+        --green: #33CC95;
+        --blue: #5429CC;
+        --blue-light: #6933ff;
+
+    }
+
+
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    body {
+        background: var(--background);
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+    
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
+    }
+
+
+    button {
+        cursor: pointer;
+    }
+
+    [disabled] {
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+
+`
